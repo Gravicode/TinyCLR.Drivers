fbb9a3b00091a610a3a035accbe41941853ba464
Mif Masterz <mifmasterz@yahoo.com>
2020-09-11 19:13:26 +0700

    add seven segment

---
+++ src/Displays.Led.FourDigitSevenSegment/FourDigitSevenSegment.cs
@@ -0,0 +1,224 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using Meadow.TinyCLR.Core.Displays;
+using System;
+using System.Threading;
+
+namespace Meadow.TinyCLR.Displays.Led
+{
+    /// <summary>
+    /// Four Digit Seven Segment Display
+    /// </summary>
+    public class FourDigitSevenSegment
+    {
+        protected bool IsAnimating = false;
+        
+
+        #region Member variables / fields
+
+        protected GpioPin[] digits;
+
+        protected SevenSegment[] sevenSegments;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Creates a SevenSegment connected to the especified ints to a IODevice
+        /// </summary>
+        /// <param name="device"></param>
+        /// <param name="pinA"></param>
+        /// <param name="pinB"></param>
+        /// <param name="pinC"></param>
+        /// <param name="pinD"></param>
+        /// <param name="pinE"></param>
+        /// <param name="pinF"></param>
+        /// <param name="pinG"></param>
+        /// <param name="pinDecimal"></param>
+        /// <param name="isCommonCathode"></param>
+        public FourDigitSevenSegment(
+          
+            int pinDigit1, int pinDigit2,
+            int pinDigit3, int pinDigit4,
+            int pinA, int pinB,
+            int pinC, int pinD,
+            int pinE, int pinF,
+            int pinG, int pinDecimal,
+            bool isCommonCathode) 
+             {
+            /*
+             this (
+                    device.CreateDigitalOutputPort(pinDigit1),
+                    device.CreateDigitalOutputPort(pinDigit2),
+                    device.CreateDigitalOutputPort(pinDigit3),
+                    device.CreateDigitalOutputPort(pinDigit4),
+                    device.CreateDigitalOutputPort(pinA),
+                    device.CreateDigitalOutputPort(pinB),
+                    device.CreateDigitalOutputPort(pinC),
+                    device.CreateDigitalOutputPort(pinD),
+                    device.CreateDigitalOutputPort(pinE),
+                    device.CreateDigitalOutputPort(pinF),
+                    device.CreateDigitalOutputPort(pinG),
+                    device.CreateDigitalOutputPort(pinDecimal),
+                    isCommonCathode)
+             */
+            var gpio = GpioController.GetDefault();
+            var PinDigit1 = gpio.OpenPin(pinDigit1);
+            PinDigit1.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinDigit2 = gpio.OpenPin(pinDigit2);
+            PinDigit2.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinDigit3 = gpio.OpenPin(pinDigit3);
+            PinDigit3.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinDigit4 = gpio.OpenPin(pinDigit4);
+            PinDigit4.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinA = gpio.OpenPin(pinA);
+            PinA.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinB = gpio.OpenPin(pinB);
+            PinB.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinC = gpio.OpenPin(pinC);
+            PinC.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinD = gpio.OpenPin(pinD);
+            PinD.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinE = gpio.OpenPin(pinE);
+            PinE.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinF = gpio.OpenPin(pinF);
+            PinF.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinG = gpio.OpenPin(pinG);
+            PinG.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinDecimal = gpio.OpenPin(pinDecimal);
+            PinDecimal.SetDriveMode(GpioPinDriveMode.Output);
+
+            Setup( PinDigit1,  PinDigit2,
+             PinDigit3,  PinDigit4,
+             PinA, PinB,
+             PinC, PinD,
+             PinE, PinF,
+             PinG, PinDecimal,
+             isCommonCathode);
+        }
+
+        /// <summary>
+        /// Creates a SevenSegment connected to the especified GpioPins
+        /// </summary>
+        /// <param name="portA"></param>
+        /// <param name="portB"></param>
+        /// <param name="portC"></param>
+        /// <param name="portD"></param>
+        /// <param name="portE"></param>
+        /// <param name="portF"></param>
+        /// <param name="portG"></param>
+        /// <param name="portDecimal"></param>
+        /// <param name="isCommonCathode"></param>
+        public FourDigitSevenSegment(
+            GpioPin portDigit1, GpioPin portDigit2,
+            GpioPin portDigit3, GpioPin portDigit4,
+            GpioPin portA, GpioPin portB,
+            GpioPin portC, GpioPin portD,
+            GpioPin portE, GpioPin portF,
+            GpioPin portG, GpioPin portDecimal, 
+            bool isCommonCathode)
+        {
+           Setup( portDigit1,  portDigit2,
+             portDigit3,  portDigit4,
+             portA,  portB,
+             portC,  portD,
+             portE,  portF,
+             portG,  portDecimal,
+             isCommonCathode);
+        }
+        void Setup(GpioPin portDigit1, GpioPin portDigit2,
+            GpioPin portDigit3, GpioPin portDigit4,
+            GpioPin portA, GpioPin portB,
+            GpioPin portC, GpioPin portD,
+            GpioPin portE, GpioPin portF,
+            GpioPin portG, GpioPin portDecimal,
+            bool isCommonCathode)
+        {
+            AnimationInfo = new Animation();
+            digits = new GpioPin[4];
+            digits[0] = portDigit1;
+            digits[1] = portDigit2;
+            digits[2] = portDigit3;
+            digits[3] = portDigit4;
+
+            sevenSegments = new SevenSegment[4];
+            for (int i = 0; i < 4; i++)
+            {
+                sevenSegments[i] = new SevenSegment(portA, portB, portC, portD, portE, portF, portG, portDecimal, isCommonCathode);
+            }
+
+
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Displays the especified valid character
+        /// </summary>
+        /// <param name="character"></param>
+        /// <param name="showDecimal"></param>
+        public void SetDisplay(char[] character, bool showDecimal = false)
+        {
+            if (IsAnimating)
+            {
+                return;
+            }
+            AnimationInfo.character = character;
+            AnimationInfo.showDecimal = showDecimal;
+            IsAnimating = true;
+            var tsk = new Thread(new ThreadStart(StartDisplayLoop));
+            tsk.Start();
+            //Task.Run(async ()=> await StartDisplayLoop(character, showDecimal, cts.Token));
+        }
+
+        protected void StartDisplayLoop() 
+        {
+            var character = AnimationInfo.character;
+            var showDecimal = AnimationInfo.showDecimal;
+            while (true)
+            {
+                if (!IsAnimating)
+                {
+                    break;
+                }
+
+                for (int i = 0; i < 4; i++)
+                {
+                    sevenSegments[i].SetDisplay(character[i], showDecimal);
+
+                    digits[i].Write( GpioPinValue.Low);
+                    digits[i].Write( GpioPinValue.High);
+                }
+
+                Thread.Sleep(7);
+            }
+        }
+
+        public void Stop()
+        {
+            IsAnimating = false;
+        }
+
+        #endregion
+        public Animation AnimationInfo { get; set; }
+        public class Animation
+        {
+            public char[] character;
+            public bool showDecimal;
+        }
+    }
+
+   
+}
---
+++ src/Meadow.TinyCLR.Core/Displays/SevenSegment.cs
@@ -0,0 +1,327 @@
+﻿using GHIElectronics.TinyCLR.Devices.Gpio;
+using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace Meadow.TinyCLR.Core.Displays
+{
+    public class SevenSegment
+    {
+        private  GpioPin _portA;
+        private  GpioPin _portB;
+        private  GpioPin _portC;
+        private  GpioPin _portD;
+        private  GpioPin _portE;
+        private  GpioPin _portF;
+        private  GpioPin _portG;
+        private  GpioPin _portDecimal;
+        private  bool _isCommonCathode;
+        private byte[,] _segments = new byte[17, 7]
+        {
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0
+      },
+      {
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0
+      },
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 1
+      },
+      {
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0
+      },
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 0,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0
+      },
+      {
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 0,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 1,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 1,
+        (byte) 1,
+        (byte) 1
+      },
+      {
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0,
+        (byte) 0
+      }
+        };
+
+        private SevenSegment()
+        {
+        }
+
+        public SevenSegment(
+      
+          int pinA,
+          int pinB,
+          int pinC,
+          int pinD,
+          int pinE,
+          int pinF,
+          int pinG,
+          int pinDecimal,
+          bool isCommonCathode)
+         
+        {
+            // : this(device.CreateDigitalOutputPort(pinA), device.CreateDigitalOutputPort(pinB), device.CreateDigitalOutputPort(pinC), device.CreateDigitalOutputPort(pinD), device.CreateDigitalOutputPort(pinE), device.CreateDigitalOutputPort(pinF), device.CreateDigitalOutputPort(pinG), device.CreateDigitalOutputPort(pinDecimal), isCommonCathode)
+            var gpio = GpioController.GetDefault();
+            var PinA = gpio.OpenPin(pinA);
+            PinA.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinB = gpio.OpenPin(pinB);
+            PinB.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinC = gpio.OpenPin(pinC);
+            PinC.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinD = gpio.OpenPin(pinD);
+            PinD.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinE = gpio.OpenPin(pinE);
+            PinE.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinF = gpio.OpenPin(pinF);
+            PinF.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinG = gpio.OpenPin(pinG);
+            PinG.SetDriveMode(GpioPinDriveMode.Output);
+
+            var PinDecimal = gpio.OpenPin(pinDecimal);
+            PinDecimal.SetDriveMode(GpioPinDriveMode.Output);
+
+            Setup(PinA,
+          PinB,
+          PinC,
+          PinD,
+          PinE,
+          PinF,
+          PinG,
+          PinDecimal,
+           isCommonCathode);
+        }
+
+        public SevenSegment(
+          GpioPin portA,
+          GpioPin portB,
+          GpioPin portC,
+          GpioPin portD,
+          GpioPin portE,
+          GpioPin portF,
+          GpioPin portG,
+          GpioPin portDecimal,
+          bool isCommonCathode)
+        {
+            Setup( portA,
+          portB,
+          portC,
+          portD,
+          portE,
+          portF,
+          portG,
+          portDecimal,
+           isCommonCathode);
+        }
+        void Setup(GpioPin portA,
+          GpioPin portB,
+          GpioPin portC,
+          GpioPin portD,
+          GpioPin portE,
+          GpioPin portF,
+          GpioPin portG,
+          GpioPin portDecimal,
+          bool isCommonCathode)
+        {
+            this._portA = portA;
+            this._portB = portB;
+            this._portC = portC;
+            this._portD = portD;
+            this._portE = portE;
+            this._portF = portF;
+            this._portG = portG;
+            this._portDecimal = portDecimal;
+            this._isCommonCathode = isCommonCathode;
+        }
+        public void SetDisplay(SevenSegment.CharacterType character, bool showDecimal = false)
+        {
+            this._portDecimal.Write((this._isCommonCathode ? showDecimal : !showDecimal) ? GpioPinValue.High : GpioPinValue.Low);
+            int index = (int)character;
+            this._portA.Write((this.IsEnabled(this._segments[index, 0])) ? GpioPinValue.High : GpioPinValue.Low );
+            this._portB.Write((this.IsEnabled(this._segments[index, 1])) ? GpioPinValue.High : GpioPinValue.Low );
+            this._portC.Write((this.IsEnabled(this._segments[index, 2])) ? GpioPinValue.High : GpioPinValue.Low );
+            this._portD.Write((this.IsEnabled(this._segments[index, 3])) ? GpioPinValue.High : GpioPinValue.Low );
+            this._portE.Write((this.IsEnabled(this._segments[index, 4])) ? GpioPinValue.High : GpioPinValue.Low );
+            this._portF.Write((this.IsEnabled(this._segments[index, 5])) ? GpioPinValue.High : GpioPinValue.Low );
+            this._portG.Write((this.IsEnabled(this._segments[index, 6])) ? GpioPinValue.High : GpioPinValue.Low );
+        }
+
+        private bool IsEnabled(byte value) => !this._isCommonCathode ? value == (byte)0 : value == (byte)1;
+
+        public void SetDisplay(char character, bool showDecimal = false)
+        {
+            SevenSegment.CharacterType character1;
+            if (character == ' ')
+                character1 = SevenSegment.CharacterType.Blank;
+            else if (character >= '0' && character <= '9')
+            {
+                character1 = (SevenSegment.CharacterType)((int)character - 48);
+            }
+            else
+            {
+                if (character < 'a' || character > 'f')
+                    throw new ArgumentOutOfRangeException();
+                character1 = (SevenSegment.CharacterType)((int)character - 97 + 10);
+            }
+            this.SetDisplay(character1, showDecimal);
+        }
+
+        public enum CharacterType
+        {
+            Zero,
+            One,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            A,
+            B,
+            C,
+            D,
+            E,
+            F,
+            Blank,
+            count,
+        }
+    }
+}
